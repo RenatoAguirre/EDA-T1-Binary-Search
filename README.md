39fa2fabeba19f8ac7fd12498c41b4a37b528504
RenatoAguirre <raaguirre1@miuandes.cl>
2024-08-15 14:21:16 -0400

    feat: added function to read from a file and instert into an array

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Analisis de Algoritmos y Tiempo de Procesamiento
 
 Para compilar el programa: (macos & linux)
+Debes estar en el directorio src
+
+$ cd src
 
 $ g++ -o out main.cpp algorithms.cpp
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include "algorithms.h"
 
+/*
+  * We read the file and insert each line into the array
+  * luckily we know how many lines the files have so we can use a fixed size array
+  * and don't have to dinamically allocate memory
+  * 
+  * @params std::string filename: the name of the file to read
+  * @params std::string array[]: the array of strings to fill
+  * @params int size: the size of the array
+  * @return void
+*/
+void read_file_and_insert_into_array(std::string filename, std::string array[], int size)
+{
+  std::ifstream file(filename);
+  if (!file) std::cerr << "Could not open the file!" << std::endl;
+
+  if (file.is_open())
+  {
+    std::cout << "File opened" << std::endl;
+    for (int i = 0; i < size; i++)
+    {
+      std::getline(file, array[i]);
+    }
+    file.close();
+  }
+}
 
 
 int main ()
 {
-  std::string array[] = {"apple", "banana", "cherry", "date", "elderberry", "fig", "grape"};
-  std::string key = "cherry";
-  int size = sizeof(array) / sizeof(array[0]);
-  
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "apple") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "banana") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "cherry") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "date") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "elderberry") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "fig") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "grape") << std::endl;
-  std::cout << string_in_array_binary_search_recursive(array, 0, size - 1, "kiwi") << std::endl;
+  const int SIZE = 10000;
+  std::string array[SIZE];
+  read_file_and_insert_into_array("../data/D100000.txt", array, SIZE);
+  std::cout << array[0] << std::endl;
   return 0;
 }
